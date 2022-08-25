ae0ded0229330be1f1f3886114cb7a14ac55d089
Jobin Thomas <90217993+jt4266@users.noreply.github.com>
2022-08-25 18:27:21 +0000

    Mitigate the Risk of Clickjacking with helmet.frameguard()

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
+const helmet = require('helmet');
 const express = require('express');
 const app = express();
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action:'deny'}));
 
 
 
